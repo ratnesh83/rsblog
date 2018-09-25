d650f32f5ba5e73c4e952e5c61e35cbe874f5eeb
Ratnesh Kumar Singh <rksdhruva@gmail.com>
2018-09-25 23:48:21 +0530

    commit

--- src/app/main/auth/auth.component.ts
+++ src/app/main/auth/auth.component.ts
@@ -8,7 +8,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class AuthComponent implements OnInit {
 
-  private index;
+  index;
   constructor(private activatedRouter: ActivatedRoute, private router: Router) {
   }
 
